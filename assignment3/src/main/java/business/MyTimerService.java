dc961b69f31a30e9b25d872953b3bdbb1b301b07
nathanwmeyer <nathanwm.meyer@gmail.com>
2021-06-03 07:35:34 -0700

    commit for assignment 4

--- assignment3/src/main/java/business/MyTimerService.java
+++ assignment3/src/main/java/business/MyTimerService.java
@@ -1,23 +1,42 @@
 package business;
 
+import java.util.logging.Logger;
+
+import javax.annotation.Resource;
 import javax.ejb.Schedule;
 import javax.ejb.Stateless;
+import javax.ejb.Timeout;
 import javax.ejb.Timer;
+import javax.ejb.TimerService;
 
 @Stateless
 public class MyTimerService {
 
+	private static final Logger logger = Logger.getLogger("business.TimerService");
+	
+	@Resource
+	TimerService timerService;
+	
     /**
      * Default constructor. 
      */
     public MyTimerService() {
         // TODO Auto-generated constructor stub
     }
+    
+    public void setTimer(long interval) {
+    	timerService.createTimer(interval, "My Timer");
+    }
+    
+    @Timeout
+    public void programmicTimer(Timer timer) {
+    	logger.info("@Timeout has occured at: " + new java.util.Date());
+    }
 	
 	@SuppressWarnings("unused")
-	@Schedule(second="*/10", minute="*", hour="8-23", dayOfWeek="Mon-Fri",
+	@Schedule(second="*/10", minute="*", hour="0-23", dayOfWeek="Mon-Fri",
       dayOfMonth="*", month="*", year="*", info="MyTimer")
     private void scheduledTimeout(final Timer t) {
-        System.out.println("@Schedule called at: " + new java.util.Date());
+        logger.info("@Schedule called at: " + new java.util.Date());
     }
 }
--- assignment3/src/main/java/controllers/FormController.java
+++ assignment3/src/main/java/controllers/FormController.java
@@ -1,11 +1,13 @@
 package controllers;
 
+import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 import javax.faces.context.FacesContext;
 import javax.inject.Inject;
 
 import beans.User;
+import business.MyTimerService;
 import business.OrdersBusinessInterface;
 
 @ManagedBean 
@@ -14,6 +16,9 @@ public class FormController {
 	
 	@Inject
 	OrdersBusinessInterface service;
+	
+	@EJB
+	MyTimerService timer;
 
 	public String onSubmit(User user)//submit user's name using the Submit Button
 	{
@@ -22,14 +27,16 @@ public class FormController {
 		user = context.getApplication().evaluateExpressionGet(context, "#{user}", User.class);
 		FacesContext.getCurrentInstance().getExternalContext().getRequestMap().put("user", user);
 		service.test();
+		timer.setTimer(10000);
 		return "TestResponse.xhtml";
 	}
-	public String onFlash(User user)//flshg user's name using the Flash Button
+	public String onFlash(User user)//flash user's name using the Flash Button
 	{
 		FacesContext context = FacesContext.getCurrentInstance();
 		user = context.getApplication().evaluateExpressionGet(context, "#{user}", User.class);
 		FacesContext.getCurrentInstance().getExternalContext().getFlash().put("user", user);
 		service.test();
+		timer.setTimer(10000);
 		return "TestResponse2.xhtml";
 	}
 	
